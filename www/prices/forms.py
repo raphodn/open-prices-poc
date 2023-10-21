8787a07589dc76fa49e93556a9b14002e70aa13a
Raphael Odini <raphodn@users.noreply.github.com>
2023-10-22 00:13:20 +0200

    Create Price model (#3)
    * Price: create model
    
    * Add Price.location_name. Update form
    
    * Register in admin
    
    * Home: display price count

--- www/prices/forms.py
+++ www/prices/forms.py
@@ -2,11 +2,16 @@ from datetime import date
 
 from django import forms
 
+from prices.models import Price
 
-class PriceCreateForm(forms.Form):
-    product_code = forms.CharField(label="Product code", min_length=13, max_length=13)
-    price = forms.CharField(label="Price")
+
+class PriceCreateForm(forms.ModelForm):
+    product_code = forms.CharField(min_length=13, max_length=13)
     currency = forms.ChoiceField(choices=[("€", "€")], initial="€", disabled=True)
-    location = forms.CharField(widget=forms.widgets.Textarea(attrs={"rows": 2, "readonly": "readonly"}))
+    location_name = forms.CharField(widget=forms.widgets.Textarea(attrs={"rows": 2, "readonly": "readonly"}))
     location_osm_id = forms.IntegerField(label="OSM ID", widget=forms.widgets.NumberInput(attrs={"readonly": "readonly"}))
     date = forms.DateField(initial=date.today, widget=forms.widgets.DateInput(attrs={"class": "form-control", "type": "date"}))
+
+    class Meta:
+        model = Price
+        fields = ["product_code", "price", "currency", "location_name", "location_osm_id", "date"]
