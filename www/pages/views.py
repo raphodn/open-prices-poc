5ebce9489a8d47994e3524d547b385be241940e5
Raphael Odini <raphodn@users.noreply.github.com>
2023-10-22 00:10:55 +0200

    Home: display price count

--- www/pages/views.py
+++ www/pages/views.py
@@ -1,5 +1,12 @@
-from django.views.generic import DetailView, FormView, ListView, TemplateView, View
+from django.views.generic import TemplateView
+
+from prices.models import Price
 
 
 class HomeView(TemplateView):
     template_name = "www/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["price_count"] = Price.objects.count()
+        return context
