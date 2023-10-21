565bbd0ee0715ccddad72dbb6f2428c355c1e8d7
Raphael Odini <raphodn@users.noreply.github.com>
2023-10-21 23:45:11 +0200

    Price: create model

--- prices/models.py
+++ prices/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Price(models.Model):
+    CURRENCY_EURO = "€"
+    CURRENCY_CHOICES = [(CURRENCY_EURO, CURRENCY_EURO)]
+
+    product_code = models.CharField(verbose_name="Product code")
+    price = models.DecimalField(verbose_name="Price", max_digits=10, decimal_places=2)
+    currency = models.CharField(verbose_name="Currency", choices=CURRENCY_CHOICES, default=CURRENCY_EURO)
+    location_osm_id = models.PositiveIntegerField(verbose_name="Location (OSM ID)")
+    date = models.DateField(verbose_name="Date")
+
+    created = models.DateTimeField(verbose_name="Creation date", default=timezone.now)
+
+    class Meta:
+        verbose_name = "Price"
+        verbose_name_plural = "Prices"
