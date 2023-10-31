d5d1b8abb5c36d75073189d743aa3ecf113665c7
Raphael Odini <raphodn@users.noreply.github.com>
2023-10-31 11:34:22 +0100

    Price API: allow create

--- api/prices/views.py
+++ api/prices/views.py
@@ -5,7 +5,7 @@ from api.prices.serializers import PriceSerializer
 from prices.models import Price
 
 
-class PriceViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class PriceViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Price.objects.all()
     serializer_class = PriceSerializer
     filterset_class = PriceFilter
