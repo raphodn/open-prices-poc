0d9079c4810d17f07765f98eda16f223bafbf06a
Raphael Odini <raphodn@users.noreply.github.com>
2023-10-31 15:43:07 +0100

    Price: add fields to store OFF metadata

--- prices/models.py
+++ prices/models.py
@@ -9,10 +9,24 @@ class Price(models.Model):
     SOURCE_API = "API"
     SOURCE_CHOICES = [(SOURCE_FORM, "Form"), (SOURCE_API, "API")]
 
+    OFF_SOURCE_OFF = "OFF"
+    OFF_SOURCE_OPF = "OPF"
+    OFF_SOURCE_OPFF = "OPFF"
+    OFF_SOURCE_OBF = "OBF"
+    OFF_SOURCE_CHOICES = [
+        (OFF_SOURCE_OFF, "OpenFoodFacts"),
+        (OFF_SOURCE_OPF, "OpenProductFacts"),
+        (OFF_SOURCE_OPFF, "OpenPetFoodFacts"),
+        (OFF_SOURCE_OBF, "OpenBeautyFacts"),
+    ]
+
     SERIALIZED_FIELDS = ["product_code", "price", "currency", "location_osm_id", "location_name", "date", "created"]
     READONLY_FIELDS = ["source", "created"]
 
     product_code = models.CharField(verbose_name="Product code")
+    product_in_off = models.BooleanField(verbose_name="Product in OFF?", blank=True, null=True)
+    product_off_db = models.CharField(verbose_name="Product OFF DB", choices=OFF_SOURCE_CHOICES, blank=True, null=True)
+    product_off_name = models.CharField(verbose_name="Product name (OFF)", blank=True, null=True)
 
     price = models.DecimalField(verbose_name="Price", max_digits=10, decimal_places=2)
     currency = models.CharField(verbose_name="Currency", choices=CURRENCY_CHOICES, default=CURRENCY_EURO)
