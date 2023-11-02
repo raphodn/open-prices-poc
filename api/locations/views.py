409def2b0415331761884175146da7e13214cb57
Raphael Odini <raphodn@users.noreply.github.com>
2023-11-02 14:24:21 +0100

    Fix typo. Add query optimisation. ref #13

--- api/locations/views.py
+++ api/locations/views.py
@@ -4,6 +4,6 @@ from api.locations.serializers import LocationSerializer
 from locations.models import Location
 
 
-class ProductViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class LocationViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
--- api/prices/views.py
+++ api/prices/views.py
@@ -6,7 +6,7 @@ from prices.models import Price
 
 
 class PriceViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
-    queryset = Price.objects.all()
+    queryset = Price.objects.select_related("product", "location").all()
     serializer_class = PriceSerializer
     filterset_class = PriceFilter
 
