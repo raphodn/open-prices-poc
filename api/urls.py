b518dba146b66b058d15f8fe0d2bddce8c5809c3
Raphael Odini <raphodn@users.noreply.github.com>
2023-11-02 14:21:36 +0100

    Add locations API endpoint. ref #13

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework import routers
 
+from api.locations.views import LocationViewSet
 from api.prices.views import PriceViewSet
 from api.products.views import ProductViewSet
 
@@ -11,6 +12,7 @@ app_name = "api"
 router = routers.DefaultRouter()
 router.register(r"prices", PriceViewSet, basename="prices")
 router.register(r"products", ProductViewSet, basename="products")
+router.register(r"locations", LocationViewSet, basename="locations")
 
 urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
