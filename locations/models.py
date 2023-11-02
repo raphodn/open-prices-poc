70e4b1ecf57ab7d037cfe857b8bc4445c954f773
Raphael Odini <raphodn@users.noreply.github.com>
2023-11-02 13:52:31 +0100

    Fetch location data on create

--- locations/models.py
+++ locations/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils import timezone
 
 
@@ -29,3 +31,11 @@ class Location(models.Model):
         unique_together = ["osm_id", "osm_type"]
         verbose_name = "Location"
         verbose_name_plural = "Locations"
+
+
+@receiver(post_save, sender=Location)
+def product_post_create_fetch_info(sender, instance, created, **kwargs):
+    if created:
+        from locations.tasks import fetch_product_info_from_openstreetmap
+
+        fetch_product_info_from_openstreetmap(instance)
