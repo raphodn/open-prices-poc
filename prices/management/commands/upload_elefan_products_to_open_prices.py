ccec7718674da3102f876bc91a0c9628fb716dbd
Raphael Odini <raphodn@users.noreply.github.com>
2023-11-28 23:30:48 +0100

    Improve stuff

--- prices/management/commands/upload_elefan_products_to_open_prices.py
+++ prices/management/commands/upload_elefan_products_to_open_prices.py
@@ -6,8 +6,12 @@ from django.core.management.base import BaseCommand
 from common.api import openfoodfacts
 
 
-ELEFAN_PRODUCTS_FILE = "local_data/20231123_elefan_products.json"
+ELEFAN_OSM_ID = 1392117416
+ELEFAN_OSM_TYPE = "NODE"
+ELEFAN_PRODUCTS_FILE = "local_data/20231127_elefan_products.json"
 FAMILLE_TO_EXCLUDE_MIN = 93
+FAMILLE_TO_EXCLUDE_EXTRA = [43, 44, 73]
+PRICE_DATE = "2023-11-27"
 
 
 class Command(BaseCommand):
@@ -27,21 +31,23 @@ class Command(BaseCommand):
         elefan_products_with_ean_13 = [p for p in elefan_products if (len(str(p["code"])) == 13)]
         print("Nombre d'articles avec EAN 13:", len(elefan_products_with_ean_13))
         elefan_products_with_ean_13_filtered = [
-            p for p in elefan_products_with_ean_13 if (p["famille"]["id"] < FAMILLE_TO_EXCLUDE_MIN)
+            p
+            for p in elefan_products_with_ean_13
+            if ((p["famille"]["id"] < FAMILLE_TO_EXCLUDE_MIN) and (p["famille"]["id"] not in FAMILLE_TO_EXCLUDE_EXTRA))
         ]
         print("Nombre d'articles uploadables avec EAN 13:", len(elefan_products_with_ean_13_filtered))
 
         progress = 0
-        for index, elefan_price in enumerate(elefan_products_with_ean_13_filtered[1:20]):
+        for index, elefan_price in enumerate(elefan_products_with_ean_13_filtered[100:]):
             print("=================")
             try:
                 PRICE = {
                     "product_code": str(elefan_price["code"]),
-                    "price": str(elefan_price["prix_vente"]),
+                    "price": elefan_price["prix_vente"],
                     "currency": "EUR",
-                    "location_osm_id": 1392117416,
-                    "location_osm_type": "NODE",
-                    "date": "2023-11-23",
+                    "location_osm_id": ELEFAN_OSM_ID,
+                    "location_osm_type": ELEFAN_OSM_TYPE,
+                    "date": PRICE_DATE,
                 }
                 print(PRICE)
                 response = openfoodfacts.create_price(price=PRICE)
@@ -49,8 +55,9 @@ class Command(BaseCommand):
                 print(response.content)
                 time.sleep(1)
                 progress += 1
-                if (progress % 10) == 0:
+                if (progress % 50) == 0:
                     print(f"{progress}...")
+                    time.sleep(10)
             except Exception as e:
                 print("Error for price index", index)
                 print(e)
